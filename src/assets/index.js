0b4705568510504bc0d960c7c3d68c2f96a95fe3
Selvan-S <selvan0023@gmail.com>
2024-08-22 01:23:51 +0530

    porfolio files

---
+++ src/assets/index.js
@@ -0,0 +1,78 @@
+import backend from "./backend.png";
+import close from "./close.svg";
+import creator from "./creator.png";
+import github from "./github.png";
+import logo from "./logo.png";
+import menu from "./menu.svg";
+import mobile from "./mobile.png";
+import web from "./web.png";
+
+import bookstore from "./bookstore.jpg";
+import carrent from "./carrent.png";
+import devbee from "./company/devbee.jpg";
+import emf from "./company/emf.png";
+import meta from "./company/meta.png";
+import samturbo from "./company/samturbo.jpg";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import v3data from "./company/v3data.jpg";
+import flowmazon from "./Flowmazon.png";
+import jobit from "./jobit.png";
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import golang from "./tech/golang.svg";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import typescript from "./tech/typescript.png";
+import tripguide from "./tripguide.png";
+import zoomcar_desktop from "./zoomcar_desktop.png";
+import zoomcar_mobile from "./zoomcar_mobile.png";
+
+export {
+  backend,
+  bookstore,
+  carrent,
+  close,
+  creator,
+  css,
+  devbee,
+  docker,
+  emf,
+  figma,
+  flowmazon,
+  git,
+  github,
+  golang,
+  html,
+  javascript,
+  jobit,
+  logo,
+  menu,
+  meta,
+  mobile,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  samturbo,
+  shopify,
+  starbucks,
+  tailwind,
+  tesla,
+  threejs,
+  tripguide,
+  typescript,
+  v3data,
+  web,
+  zoomcar_desktop,
+  zoomcar_mobile,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,290 @@
+import {
+  backend,
+  bookstore,
+  carrent,
+  css,
+  devbee,
+  docker,
+  emf,
+  flowmazon,
+  git,
+  golang,
+  html,
+  javascript,
+  jobit,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  samturbo,
+  tailwind,
+  threejs,
+  tripguide,
+  typescript,
+  v3data,
+  web,
+  zoomcar_mobile,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "Go",
+    icon: golang,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Assembly Engineer",
+    company_name: "EMF Innovations",
+    icon: emf,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - Oct 2022",
+    points: [
+      "Inspected completed EV motor parts and components for quality control purposes.",
+      "Collaborated with other line workers and team members to assemble and repair parts.",
+      "Achieve the daily production target while ensuring the motor's functionality is thoroughly inspected.",
+    ],
+  },
+  {
+    title: "Marketing Executive",
+    company_name: "Sam Turbo Industry",
+    icon: samturbo,
+    iconBg: "#383E56",
+    date: "Dec 2022 - Jul 2023",
+    points: [
+      "Ensure ongoing communication between our marketing team and the client's marketing team regarding product design.",
+      "We aim to obtain the client's design approval within the designated timeframe of five days, in order to proceed to the manufacturing phase of the product.",
+    ],
+  },
+  {
+    title: "Lead Generation Executive",
+    company_name: "V3 Data Intelligence",
+    icon: v3data,
+    iconBg: "#E6DEDD",
+    date: "Feb 2024 - May 2024",
+    points: [
+      "Utilize email and LinkedIn platforms to effectively conduct marketing campaigns aimed at generating leads.",
+      "Email marketing campaigns that nurtured prospects through the sales funnel, resulting in greater conversions from leads to customers.",
+      "Utilized data-driven insights to refine targeting strategies, increasing qualified leads generated.",
+    ],
+  },
+  {
+    title: "Full-Stack Developer",
+    company_name: "DevBee Inc",
+    icon: devbee,
+    iconBg: "#383E56",
+    date: "June 2024 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Zoomcar Clone",
+    description:
+      "Zoomcar Clone is a car rental application designed to provide users with a seamless vehicle rental experience. Users can search and filter vehicles based on categories and budget, view availability and price details, manage bookings, and process secure payments. The app ensures a user-friendly interface, secure access, and comprehensive booking management",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "expressjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "daisyui",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: zoomcar_mobile,
+    source_code_link: "https://github.com/Selvan-S/zoomcar-clone-frontend",
+  },
+  {
+    name: "Flowmazon",
+    description:
+      "The E-commerce application was created to gain practical knowledge of Next.js' new app router, server actions, and the complexities of working with the Prisma database.",
+    tags: [
+      {
+        name: "Nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TypeScript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Next-Auth",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Prisma",
+        color: "white-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "daisyui",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: flowmazon,
+    source_code_link: "https://github.com/Selvan-S/nextjs-ecommerce",
+  },
+  {
+    name: "Bookstore",
+    description:
+      "The Bookstore web application is specifically created to handle book records. Users have the ability to add, edit, and delete their books. Furthermore, users can provide reviews for books added by other users.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "expressjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: bookstore,
+    source_code_link: "https://github.com/Selvan-S/bookstore-frontend",
+  },
+];
+
+export { experiences, projects, services, technologies, testimonials };
